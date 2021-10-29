faf9ebf38eab94766724471bbd9efd91f51df4ec
Mari Franco <marifranco@Maris-Air.home>
2021-10-29 13:22:22 -0700

    fix

---
+++ src/components/Education/Education.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import classes from './Education.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+import { MdSchool } from 'react-icons/md';
+import { MdWork } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
+
+class Education extends Component {
+    render() {
+        return (
+                <div className={classes.box} id="education">
+                    <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2' animateOnce="true" initiallyVisible ={true}>
+                     <span  className={classes.head}>MY JOURNEY</span>
+                    <section className={classes.container}>
+                        <div className={classes.container_content}>
+                            <div className={classes.row}>
+                                <div className={classes.row_md_12}>
+                                    <div className={classes.timeline_centered}>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article className={classes.timeline_entry}>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`} >
+                                                    <FaSchool />     
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 >Higher Education  <span>2014-2016</span></h2>
+                                                <p>I have completed my higher education from UCSC with major in Legal Studies </p>
+                                                </div>
+                                            </article>
+                                        </ScrollAnimation>
+                                       <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article className={classes.timeline_entry}>
+                                                <div className={classes.timeline_icon} >
+                                                <MdSchool />
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 >current Student <span>Present</span></h2>
+                                                <p>I'm currently completing my coding certificate from <a href='http://TRILIOGYLINKHERE'>Trilogy</a>. </p>
+                                                </div>
+                                            </article>
+                                        </ScrollAnimation>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                            <article>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`} >
+                                                    <Work />     
+                                                </div>
+                                                <div className={classes.label}>
+                                                <h2 >pending</h2>
+                                                <p>pending</p>
+                                                </div>
+                                                <div className={classes.timeline_entry_inner}><div  className={classes.timeline_icon_3||classes.color_none}></div></div>
+                                            </article>
+                                        </ScrollAnimation>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    </ScrollAnimation>
+                </div>
+     
+        )
+    }
+}
+
+export default Education;
